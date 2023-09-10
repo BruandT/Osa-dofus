ba5989fc9550f7542791513c261b8b7c0f8b141d
Thomas Bruand <thomas.bruand@gmail.com>
2023-09-10 17:44:01 +0200

    Ajout de l'image du monstre, ajout de .map sur les drops + ajout des images des items

--- src/components/CardMonster.jsx
+++ src/components/CardMonster.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 function CardMonster(data) {
+  const itemsArray = Object.entries(data.item); // Convertir les valeurs de l'objet en tableau
+  const itemsToShow = itemsArray.slice(0, 7); // Obtenez les 8 premières entrées
+
   return (
     <>
       <div className='w-64 h-64 flex flex-col items-center bg-skin-bg-color/50 border-solid border-2 border-skin-light rounded'>
         <div className='w-full mt-2 flex '>
           <img
-            src='./assets/images/Dofus_emeraude.png'
-            alt='image du monstre'
+            src={`./assets/sprites/monsters/80/${data.id}.png`}
+            alt={data.name}
             className=' w-20 h-20 ml-3'
           />
           <div className='flex flex-col mt-2'>
@@ -17,15 +20,23 @@ function CardMonster(data) {
             </p>
           </div>
         </div>
-        <div className='w-full mt-4 py-1 flex flex-wrap justify-center gap-1'>
-          <div className='h-14 w-14 bg-skin-bg-color border rounded'></div>
-          <div className='h-14 w-14 bg-skin-bg-color border rounded'></div>
-          <div className='h-14 w-14 bg-skin-bg-color border rounded'></div>
-          <div className='h-14 w-14 bg-skin-bg-color border rounded'></div>
-          <div className='h-14 w-14 bg-skin-bg-color border rounded'></div>
-          <div className='h-14 w-14 bg-skin-bg-color border rounded'></div>
-          <div className='h-14 w-14 bg-skin-bg-color border rounded'></div>
-          <div className='h-14 w-14 bg-skin-bg-color border rounded'></div>
+        <div className='w-full mt-6 py-1 flex flex-wrap justify-center gap-1'>
+          {itemsToShow.map(([itemId]) => (
+            <div
+              key={itemId}
+              className='h-14 w-14 bg-skin-bg-color border rounded'
+            >
+              <img
+                src={`./assets/sprites/items/200/${itemId}.png`}
+                alt={itemId}
+                className='w-full h-full'
+              />
+            </div>
+          ))}
+
+          {itemsArray.length > 7 && (
+            <div className='bg-addition h-14 w-14 border rounded flex items-center justify-center text-2xl font-bold'></div>
+          )}
         </div>
       </div>
     </>
