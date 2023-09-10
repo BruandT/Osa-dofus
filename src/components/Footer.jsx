451d4a7dd5aaf25e95b2c3e9ed0dd76c883a17db
Thomas Bruand <thomas.bruand@gmail.com>
2023-09-10 17:45:26 +0200

    Ajout du style du component Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,21 +3,30 @@ import React from "react";
 function Footer(props) {
   return (
     <>
-      <footer className="h-24 bg-skin-bg-shadow flex flex-col items-center">
-        <div className="w-11/12 h-full flex flex-col justify-center items-center">
-        <p className="text-white">
-          Dofus est un MMORPG édité par <a target="_blank" href="https://www.ankama.com/fr" className="text-skin-color">Ankama</a>. " Osa'loot " est un site
-          non-officiel sans aucun lien avec Ankama. Toutes les illustrations
-          sont la propriété d'Ankama Studio et de Dofus.
-        </p>
-        <a
-          href='https://github.com/BruandT'
-          target='_blank'
-          rel='noopener noreferrer'
-          className="text-skin-color"
-        >
-          Ymokay
-        </a>
+      <footer className='h-24 bg-skin-bg-shadow flex flex-col items-center'>
+        <div className='w-11/12 h-full flex flex-col justify-center items-center'>
+          <p className='text-white'>
+            Dofus est un MMORPG édité par{" "}
+            <a
+              target='_blank'
+              href='https://www.ankama.com/fr'
+              rel='noopener noreferrer'
+              className='text-skin-color'
+            >
+              Ankama
+            </a>
+            . " Osa'loot " est un site non-officiel sans aucun lien avec Ankama.
+            Toutes les illustrations sont la propriété d'Ankama Studio et de
+            Dofus.
+          </p>
+          <a
+            href='https://github.com/BruandT'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-skin-color'
+          >
+            Ymokay
+          </a>
         </div>
       </footer>
     </>
